4e026c1fe4589c00bb9f0bf471a414e0b6848884
= <=>
2026-01-04 12:34:36 +0100

    fix nix link script

--- scripts/create-nixos-symlinks.bash
+++ scripts/create-nixos-symlinks.bash
@@ -1,5 +1,21 @@
 sudo rm /etc/nixos/configuration.nix
 sudo rm /etc/nixos/hardware-configuration.nix
 
-sudo ln -s ~/nixos-cfg/nixos/configuration.nix /etc/nixos/configuration.nix
-sudo ln -s ~/nixos-cfg/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+if [ ! -d "~/nixos-cfg/hosts/$(hostname)" ]; then
+    echo "Host directory does not exist"
+    exit 1
+fi
+
+if [ ! -f "~/nixos-cfg/hosts/$(hostname)/configuration.nix" ]; then
+    echo "Configuration file does not exist"
+    exit 1
+else
+    sudo ln -s ~/nixos-cfg/hosts/$(hostname)/configuration.nix /etc/nixos/configuration.nix
+fi
+
+if [ ! -f "~/nixos-cfg/hosts/$(hostname)/hardware-configuration.nix" ]; then
+    echo "Hardware configuration file does not exist"
+    exit 1
+else   
+    sudo ln -s ~/nixos-cfg/hosts/$(hostname)/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+fi
